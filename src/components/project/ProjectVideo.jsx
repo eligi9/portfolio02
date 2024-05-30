6e17ba1de16051249fab9e0df1ceaba133e22559
Elias Ginter <eligi9@gmx.de>
2024-05-30 18:25:15 +0200

    Added Skeleton for IFrame

--- src/components/project/ProjectVideo.jsx
+++ src/components/project/ProjectVideo.jsx
@@ -1,20 +1,30 @@
 import GridItem from '/components/grid/GridItem';
 import PropTypes from 'prop-types';
-import { useViewportSize } from '@mantine/hooks';
+import styles from '/css/components/project/ProjectVideo.module.scss';
+import {Skeleton} from '@mantine/core';
+import { useState } from 'react';
+
 
 ProjectVideo.propTypes = {
   video: PropTypes.string
 };
 
 export default function ProjectVideo({video}) {
-  const { height, width } = useViewportSize();
+  const [loading, setLoading] = useState(true);
 
   return (
     <>
     <GridItem cols="2/9" justify={"center"}>
-    <iframe width={16/9*1/2*height} height={1/2*height} src={video} 
-    title="YouTube video player"
-    allowFullScreen></iframe>
+      {loading && <Skeleton className={styles.video} radius="xl" />}
+      <iframe 
+        src={video} 
+        title="YouTube video player"
+        allowFullScreen
+        className={styles.video}
+        onLoad={() => setLoading(false)}
+        style={{ display: loading ? 'none' : 'block'}}
+        
+      ></iframe>
     </GridItem>
     </>
   )
